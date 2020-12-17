a71d1fcc83bf2f9534b3414f1dde395124d9097f
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-12-16 23:25:31 -0800

    Multiple enhancements
    Multiple enhancements
    * Multiple hello messages (randomized)
    * Handle common phone number formats
    * Queue calls and do random sleep in between calls
    * More crawl code
    * Skip URL if already seen
    * Recursive crawl with depth param (hardcoded to go 1 deep for now)
    * More swagger

--- server/routes/callme.py
+++ server/routes/callme.py
@@ -3,9 +3,13 @@ import os
 from dotenv import load_dotenv
 from flask import jsonify
 from flask import request
+import random
 from server import app
 from server.routes import prometheus
 from twilio.rest import Client
+import threading
+import time
+import queue
 
 # Initialize the Twilio client
 #
@@ -15,8 +19,33 @@ load_dotenv()
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 from_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
+sleep_min = os.environ.get('SLEEP_MIN', 1)
+sleep_max = os.environ.get('SLEEP_MAX', 10)
 client = Client(account_sid, auth_token)
 
+# Queue and tasks for calling to add random wait times between calls
+q = queue.Queue()
+
+def caller():
+  while True:
+    callee = q.get()
+    print('Begin calling:', callee)
+    call_via_twilio(callee[0], callee[1])
+    print('Done texting "', callee[1], '" to', callee[0])
+    s = random.randint(sleep_min, sleep_max)
+    print('Wait ', s, 'seconds')
+    time.sleep(s)
+    q.task_done()
+
+threading.Thread(target=caller, daemon=True).start()
+
+# Random initial text message
+hellos = [
+  'Hi there',
+  'Howdy',
+  'Hi',
+  'Hey'
+]
 
 @app.route("/api/v1/callme", methods=['POST'])
 @prometheus.track_requests
@@ -25,10 +54,13 @@ def callme():
     input = request.get_json(force=True)
     for i in input:
         name = i.get('name')
-        phone = i['phone']
-        message = i.get('message', 'Hi there')
-        print("Call", name, "at", phone, "message:", message)
-        call_via_twilio(phone, message)
+        digits = ''.join(n for n in i['phone'] if n.isdigit())
+        prefix = '+1' if len(digits) == 10 else '+'
+        phone = prefix + digits
+        message = i.get('message', random.choice(hellos))
+        print("POST", name, "at", phone, "message:", message)
+        # call_via_twilio(phone, message)
+        q.put((phone, message))
 
     state = {"status": "Accepted"}
     return jsonify(state), 202
