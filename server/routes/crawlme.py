864e9d722a7ebc2b0ddc4a76e756e290d75cc60f
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-12-16 23:15:44 -0800

    Multiple enhancements
    * Multiple hello messages (randomized)
    * Handle common phone number formats
    * Queue calls and do random sleep in between calls
    * More crawl code
    * Skip URL if already seen
    * Recursive crawl with depth param (hardcoded to go 1 deep for now)
    * More swagger

--- server/routes/crawlme.py
+++ server/routes/crawlme.py
@@ -10,10 +10,16 @@ from ibm_watson import DiscoveryV1
 from server import app
 from server.routes import prometheus
 
+# Max crawl depth
+max_depth = 1
+
+# Create array to store URLs that we have already crawled
+crawled_urls = []
+
 # Initialize the Discovery client
 #
 load_dotenv()
-DISCOVERY_PROJECT_ID = os.environ.get('DISCOVERY_PROJECT_ID')
+DISCOVERY_COLLECTION_ID = os.environ.get('DISCOVERY_COLLECTION_ID')
 DISCOVERY_ENVIRONMENT_ID = os.environ.get('DISCOVERY_ENVIRONMENT_ID')
 discovery = DiscoveryV1(version='2020-12-08')
 
@@ -24,29 +30,48 @@ def crawlme():
     """crawlme url route"""
     input = request.get_json(force=True)
     print("INPUT:", input)
+
+    # TODO: queue these and run with threading
     crawl_url(input.get('url'))
 
     state = {"status": "Accepted"}
     return jsonify(state), 202
 
 
-def crawl_url(url):
+def crawl_url(url, depth=0):
     print("Crawlme url:", url)
+
+    if url in crawled_urls:
+        print("Skipping already crawled url:", url)
+        return
+    crawled_urls.append(url)
+
     try:
         page = requests.get(url)
     except Exception as e:
         print(e)
         return
 
-    soup = BeautifulSoup(page.text)  # , 'lxml')
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-    send_to_discovery(page.text)
+    soup = BeautifulSoup(page.content, 'lxml')
+    send_to_discovery(soup.prettify())
+
+    if depth < max_depth:
+        depth += 1
+        # recursive crawl...
+        # extract links from 'href' and 'src'
+        # future: do something intelligent to only crawl interesting ones
+        new_links = [
+            item['href'] if item.get('href') is not None else item['src']
+            for item in soup.select('[href^="http"], [src^="http"]')
+        ]
+        print('All URLs found: ', new_links)
+        for i in new_links:
+            crawl_url(i, depth)
+        # for link in soup.find_all('a'):
+        # print(link.get('href'))
 
 
 def send_to_discovery(text_io):
-
-    print("text_io type:", type(text_io))
 
     if not (DISCOVERY_COLLECTION_ID and DISCOVERY_ENVIRONMENT_ID):
         print("---> Skipping Discovery feed <--- (not configured)")
@@ -58,3 +83,21 @@ def send_to_discovery(text_io):
         file=text_io, filename="testfile").get_result()
 
     print(json.dumps(add_doc, indent=2))
+
+    # write html to file as text, use url domain-path as file name
+    # domain = urlparse(url).netloc
+
+
+# def get_filename(url):
+#
+    # # !! Need to remove all '/' and replace with '-', below doesn't work
+    # # path = urlparse(url).path.split.replace("/","-")
+    # path = urlparse(url).path.strip("/")
+    # url_as_string = str(domain + "-" + path)
+    # suffix = '.txt'
+    # save_path = os.getcwd()
+    # output_file = os.path.join(save_path, url_as_string + suffix)
+    # with open(output_file, 'w', encoding='utf-8') as file:
+        # file.write(soup.prettify())
+    # print('Output file name is ', output_file)
+    # print("\n")
